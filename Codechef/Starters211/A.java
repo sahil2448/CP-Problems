cce1207312ccd8184523a0f45325945c3299227e
Sahil Kamble <syk2448@gmail.com>
2025-11-05 21:28:16 +0530

    .

---
+++ Codechef/Starters211/A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class A {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+
+    }
+}
+}
